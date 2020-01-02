74db229245d0148ee8500492d3c098c32b126e8d
Teerapong Chantakard <azygous13@gmail.com>
2020-01-03 01:10:22 +0700

    Added first screen

--- app/src/main/java/com/cakii/sennaquestion2/MainActivity.kt
+++ app/src/main/java/com/cakii/sennaquestion2/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.cakii.sennaquestion2
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Patterns
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,25 +18,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-    }
+        spinner_gender.adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_spinner_dropdown_item,
+            arrayListOf("Please select your gender", "Male", "Female")
+        )
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+        btn_next.setOnClickListener {
+            var isValid = true
+            if (spinner_gender.selectedItemPosition == 0) {
+                isValid = false
+                val errorText = spinner_gender.selectedView as TextView
+                errorText.setTextColor(Color.RED)
+                errorText.text = getString(R.string.please_select_your_gender)
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+            }
+            if (et_first_name.text.isEmpty()) {
+                isValid = false
+                et_first_name.error = getString(R.string.first_name_cannot_be_empty)
+            }
+            if (et_last_name.text.isEmpty()) {
+                isValid = false
+                et_last_name.error = getString(R.string.last_name_cannot_be_empty)
+            }
+            if (et_email.text.isEmpty()) {
+                isValid = false
+                et_email.error = getString(R.string.email_cannot_be_empty)
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(et_email.text).matches()) {
+                isValid = false
+                et_email.error = getString(R.string.invalid_email_format)
+            }
+            if (isValid) {
+                val intent = Intent(this, DetailActivity::class.java)
+                intent.putExtra("gender", spinner_gender.selectedItem.toString())
+                intent.putExtra("first_name", et_first_name.text.toString())
+                intent.putExtra("last_name", et_last_name.text.toString())
+                intent.putExtra("email", et_email.text.toString())
+                startActivity(intent)
+            }
         }
     }
 }
